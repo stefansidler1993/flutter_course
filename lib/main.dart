d1c6f9754b3b88061a545239ae7655c4099ed06d
Stefan Sidler <stefan_sidler@claro.com.do>
2019-05-28 16:18:22 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/images/food.jpg'),
+                  Text('Food Paradise')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
