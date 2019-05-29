563fbe182c35ef4437a9630208beb924265ce72e
Stefan Sidler <stefan_sidler@claro.com.do>
2019-05-29 00:13:58 -0400

    managing data inside widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  List<String> _products = ['Food Tester'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text('Add Product'),
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
               ),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/images/food.jpg'),
-                  Text('Food Paradise')
-                ],
+              Column(
+                children: _products
+                    .map(
+                      (element) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/images/food.jpg'),
+                                Text('Food Paradise')
+                              ],
+                            ),
+                          ),
+                    )
+                    .toList(),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
