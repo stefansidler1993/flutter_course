60537c729e29f0d857109dac7c1c8d61200b99fb
Stefan Sidler <stefan_sidler@claro.com.do>
2019-05-31 14:51:28 -0400

    fixing statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() {runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp>{
   List<String> _products = ['Food Tester'];
 
   @override
@@ -19,7 +26,11 @@ class MyApp extends StatelessWidget {
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      _products.add("Advanced Food Tester");
+                    });
+                  },
                   child: Text('Add Product'),
                 ),
               ),
@@ -41,4 +52,6 @@ class MyApp extends StatelessWidget {
           )),
     );
   }
+
+
 }
